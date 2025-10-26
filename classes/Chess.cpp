e1e400c05af320de8c4bc3e8a83cd34122d4b0cc
ajfrock2 <austfrock@gmail.com>
2025-10-25 17:14:23 -0700

    Implemented FENtoBoard

--- classes/Chess.cpp
+++ classes/Chess.cpp
@@ -1,6 +1,10 @@
 #include "Chess.h"
 #include <limits>
 #include <cmath>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cctype>
 
 Chess::Chess()
 {
@@ -47,20 +51,86 @@ void Chess::setUpBoard()
 
     _grid->initializeChessSquares(pieceSize, "boardsquare.png");
     FENtoBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    
+    //FENtoBoard("8/1p3pp1/p2p4/3P4/4P3/5N2/PP3PPP/RNBQKB1R"); // Test 1
+    //FENtoBoard("r1bqkbnr/pppppppp/2n5/8/8/2N5/PPPPPPPP/R1BQKBNR"); // Test 2
 
     startGame();
 }
 
-void Chess::FENtoBoard(const std::string& fen) {
-    // convert a FEN string to a board
-    // FEN is a space delimited string with 6 fields
-    // 1: piece placement (from white's perspective)
-    // NOT PART OF THIS ASSIGNMENT BUT OTHER THINGS THAT CAN BE IN A FEN STRING
-    // ARE BELOW
-    // 2: active color (W or B)
-    // 3: castling availability (KQkq or -)
-    // 4: en passant target square (in algebraic notation, or -)
-    // 5: halfmove clock (number of halfmoves since the last capture or pawn advance)
+// Takes in a fen string and modifies the board to match it 
+// ------------------------------TODO expand logic for task 2,3,4,5------------------------------
+
+// convert a FEN string to a board
+// FEN is a space delimited string with 6 fields
+// 1: piece placement (from white's perspective)
+// NOT PART OF THIS ASSIGNMENT BUT OTHER THINGS THAT CAN BE IN A FEN STRING
+// ARE BELOW
+// 2: active color (W or B)
+// 3: castling availability (KQkq or -)
+// 4: en passant target square (in algebraic notation, or -)
+// 5: halfmove clock (number of halfmoves since the last capture or pawn advance)
+void Chess::FENtoBoard(const std::string& fen) 
+{
+    // Parsing into 8 strings
+    std::vector<std::string> rows;
+    std::stringstream ss(fen);
+    std::string item;
+    while (std::getline(ss, item, '/')) {
+        rows.push_back(item);
+    }
+
+    int rank = 0;
+    int file = 0;
+    // Iterate over each letter correlating to different square
+    for(std::string letters: rows){
+        for(char letter: letters){
+            Bit* piece;
+            int playerColor = 0; // Default white
+            int asciiVal = static_cast<int> (letter);
+
+            // If uppercase, change to black
+            if(asciiVal >= 65 && asciiVal <= 90){
+                playerColor = 1;
+                letter = std::tolower(letter);
+            }
+
+            // Get piece based on char and player number
+            switch(letter)
+            {
+                case 'p':
+                    piece = PieceForPlayer(playerColor, ChessPiece::Pawn);
+                    break;
+                case 'r':
+                    piece = PieceForPlayer(playerColor, ChessPiece::Rook);
+                    break;
+                case 'n':
+                    piece = PieceForPlayer(playerColor, ChessPiece::Knight);
+                    break;
+                case 'b':
+                    piece = PieceForPlayer(playerColor, ChessPiece::Bishop);
+                    break;
+                case 'k':
+                    piece = PieceForPlayer(playerColor, ChessPiece::King);
+                    break;
+                case 'q':
+                    piece = PieceForPlayer(playerColor, ChessPiece::Queen);
+                    break;
+                default:
+                    // Number logic
+                    int num = letter - '0';
+                    file+=num;
+                    continue;
+            }
+            // Add the piece to board
+            piece->setPosition(_grid->getSquare(file, rank)->getPosition());
+            _grid->getSquare(file, rank)->setBit(piece);
+            
+            file++;
+        }
+        rank++;
+        file=0;
+    }
 }
 
 bool Chess::actionForEmptyHolder(BitHolder &holder)
